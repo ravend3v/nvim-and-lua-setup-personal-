b03408df4d16099e474186c83e78e372ee0015f3
ravend3v <ojalaelias05@gmail.com>
2025-03-09 19:56:15 +0200

    lsp update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,8 +1,8 @@
 return {
+    -- Mason setup
     {
         "williamboman/mason.nvim",
         config = function()
-            -- setup mason with default properties
             require("mason").setup({
                 ui = {
                     border = "rounded"
@@ -10,27 +10,24 @@ return {
             })
         end,
     },
-    -- mason lsp config utilizes mason to automatically ensure lsp servers you want installed are installed
+    -- Mason LSP config
     {
         "williamboman/mason-lspconfig.nvim",
-        config = function()
-            -- ensure that we have lua language server, typescript launguage server, java language server, and java test language server are installed
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls", "jdtls", "cssls" },
-            })
-        end,
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
     },
-    -- mason nvim dap utilizes mason to automatically ensure debug adapters you want installed are installed, mason-lspconfig will not automatically install debug adapters for us
+    -- Mason DAP setup
     {
         "jay-babu/mason-nvim-dap.nvim",
         config = function()
-            -- ensure the java debug adapter is installed
             require("mason-nvim-dap").setup({
                 ensure_installed = { "java-debug-adapter", "java-test" },
             })
         end,
     },
-    -- utility plugin for configuring the java language server for us
+    -- Java language server setup
     {
         "mfussenegger/nvim-jdtls",
         dependencies = {
@@ -38,36 +35,32 @@ return {
             "ray-x/lsp_signature.nvim"
         },
     },
+    -- LSP signature setup
     {
         "ray-x/lsp_signature.nvim",
         config = function()
             require "lsp_signature".setup()
         end
     },
+    -- LSP config setup
     {
         "neovim/nvim-lspconfig",
         config = function()
             local icons = require("config.icons")
-
-            -- get access to the lspconfig plugins functions
             local lspconfig = require("lspconfig")
-
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- setup the lua language server
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+            -- Language servers to setup
+            local servers = { "lua_ls", "tsserver", "cssls" }
 
-            -- setup the typescript language server
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
+            -- Setup each language server
+            for _, server in ipairs(servers) do
+                lspconfig[server].setup({
+                    capabilities = capabilities,
+                })
+            end
 
-            lspconfig.cssls.setup({
-                capabilities = capabilities
-            })
-
+            -- Diagnostic configuration
             local default_diagnostic_config = {
                 signs = {
                     active = true,
@@ -98,29 +91,14 @@ return {
                 vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
             end
 
-            -- Set vim motion for <Space> + c + h to show code documentation about the code the cursor is currently over if available
-            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
-            -- Set vim motion for <Space> + c + d to go where the code/variable under the cursor was defined
+            -- Key mappings for LSP
+            local keymap_opts = { desc = "[C]ode [H]over Documentation" }
+            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, keymap_opts)
             vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
-            -- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
-            -- Set vim motion for <Space> + c + r to display references to the code under the cursor
-            vim.keymap.set(
-                "n",
-                "<leader>cr",
-                require("telescope.builtin").lsp_references,
-                { desc = "[C]ode Goto [R]eferences" }
-            )
-            -- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
-            vim.keymap.set(
-                "n",
-                "<leader>ci",
-                require("telescope.builtin").lsp_implementations,
-                { desc = "[C]ode Goto [I]mplementations" }
-            )
-            -- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
+            vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode Goto [R]eferences" })
+            vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations, { desc = "[C]ode Goto [I]mplementations" })
             vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
-            -- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
             vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
         end,
     },
