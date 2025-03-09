517f91b51b0c347224f2bffb42276255dcb9e24a
ravend3v <ojalaelias05@gmail.com>
2025-03-09 19:43:33 +0200

    fix

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,20 +1,20 @@
 return {
     "rmagatti/auto-session",
     config = function()
-        require("auto-session").setup ({
-           -- supress
-           auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads" },
+        require("auto-session").setup({
+            -- supress
+            auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads" },
 
-           -- sessions lens
-           session_lens = {
+            -- sessions lens
+            session_lens = {
                 buftypes_to_ignore = {},
                 load_on_setup = true,
                 theme_conf = { border = true },
                 previewer = false,
-           },
-           vim.keymap.set("n", "<Leader>ls", require("auto-session.session_lens").search_session), {
-               noremap = true,
-           }),
+            },
+        })
+        vim.keymap.set("n", "<Leader>ls", require("auto-session.session_lens").search_session, {
+            noremap = true,
         })
     end,
 }
