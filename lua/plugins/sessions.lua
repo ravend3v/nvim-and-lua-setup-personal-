22039069a20077550b13f6d1de4c19ba237446cd
ravend3v <ojalaelias05@gmail.com>
2025-03-09 19:23:18 +0200

    adding auto-sessions

---
+++ lua/plugins/sessions.lua
@@ -0,0 +1,17 @@
+return {
+    "rmagatti/auto-session",
+    config = function()
+        require("auto-session").setup ({
+           -- supress
+           auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads" },
+
+           -- sessions lens
+           sessions_lens = {
+                buftypes_to_ignore = {},
+                load_on_setup = true,
+                theme_conf = { border = true },
+                previewer = false,
+           }.
+        })
+    end,
+}
